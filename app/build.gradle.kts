a75732174c746bb1b92192c3945809c711d6f873
Yusuf Budi <yusufbudiprasetiyo02@gmail.com>
2026-06-23 10:34:30 +0700

    Rename application id for ArliDeck

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,11 @@ android {
     }
 
     defaultConfig {
-        applicationId = "app.floatdeck"
+        applicationId = "app.arlideck"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1-arli"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
